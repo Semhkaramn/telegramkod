a1650a6a1e667b196e37aecc56111ad88e5d3de4
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2026-01-31 23:27:34 +0300

    Update skeleton.tsx

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -6,7 +6,7 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-lg bg-zinc-200", className)}
+      className={cn("animate-pulse rounded-xl bg-slate-800", className)}
       {...props}
     />
   );
